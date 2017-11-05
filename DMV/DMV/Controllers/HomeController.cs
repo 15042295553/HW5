7664186e95dc081a2286aaae5dedccd8b9bd6286
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2017-11-05 09:30:16 +0800

    HW5

---
+++ DMV/DMV/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult driver()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult driver(driver c)
+        {
+            if(ModelState.IsValid)
+            {
+                using (Database1Entities dc = new Database1Entities())
+                {
+                    dc.driver.Add(c);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    c = null;
+                    ViewBag.Message = "Successfully change address";
+                }
+            }
+            return View(c);
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿create table driver
+(
+FullName int primary key,
+FirstName varchar(50),
+LastName varchar(50),
+DOB datetime,
+NewAddress varchar(225),
+City varchar(50),
+State varchar(50),
+Zip_Code varchar(50),
+county varchar(50),
+);
+
+insert into driver
+values(1,'Chunze','Liu','1996-09-16','350 Powell St E', 'Monmouth','OR','97361','Polk')
+
+
+
+
+
